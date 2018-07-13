62f45d360c0f2758ad759802d820b93dfc9e7e67
giovanni3009 <giovanni.manantsoa@gmail.com>
2018-07-13 10:53:00 +0300

    Menu Fait

---
+++ menu.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Rendre la page responsive-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+   
+    </head> 
+<body>
+
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="">PROJET TRIGGER</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      <li class="dropdown-toggle"><a href="#"><i class="glyphicon glyphicon-plus"></i> RECETTES</a></li>
+        <li class="dropdown-toggle"><a href="#"><i class="glyphicon glyphicon-user"></i> VENDEURS</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-bitcoin"></i> JOURNAL DE RECETTES<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Recettes Journalieres</a></li>
+            <li><a href="#">Recettes Mensuelles</a></li>
+            <li><a href="#">Recettes Vendeurs par Mois</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-dashboard"></i> HISTORIQUE<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Historique des Vendeurs</a></li>
+            <li><a href="#">Historique des Recettes</a></li>
+          </ul>
+        </li>
+        <li class="dropdown-toggle"><a href="#"><i class="glyphicon glyphicon-stats"></i> STATISTIQUES</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-log-out"></i> DECONNEXION<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Manantsoa Giovanni</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<script src="assets/jquery/jquery-2.1.4.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/datatables/js/jquery.dataTables.min.js"></script>
+<script src="assets/datatables/js/dataTables.bootstrap.js"></script>
+<script src="assets/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+
+</body>
+
+</html>
