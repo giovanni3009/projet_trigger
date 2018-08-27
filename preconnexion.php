a865173521e42f41d63c8f51abe3daa746845ce8
giovanni <giovanni.manantsoa@gmail.com>
2018-08-27 09:13:13 +0300

    preconnexion fait

---
+++ preconnexion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Rendre la page responsive-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+   
+    </head> 
+<body>
+<div class="container">
+    <div class="well">
+      <center>
+        <h3>BIENVENUE SUR NOTRE APPLICATION DE GESTION DES VENTES AVEC TRIGGER</h3>
+        <h4><b>VOUS-ETES :</b> </h4>
+        <a href="recette_liste.php" class="btn btn-primary btn-lg"><i class="glyphicon glyphicon-bitcoin"></i> VENDEUR</a>
+        <a href="recette_liste.php" class="btn btn-success btn-lg"><i class="glyphicon glyphicon-user"></i> ADMINISTRATEUR</a>
+      </center>
+    </div>
+<div class="container">
+</body>
